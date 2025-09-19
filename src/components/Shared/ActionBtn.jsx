34251366444d07a51bd004db21cc6e99b1e33d91
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-09-19 20:52:08 +0400

    Add action button shared component

---
+++ src/components/Shared/ActionBtn.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./ActionBtn.module.css";
+import cartIcon from "../../assets/shopping-cart-white.png";
+
+function ActionBtn({ variant, children, width, size }) {
+  return (
+    <button
+      className={size === "large" ? styles.btnLarge : styles.btnSmall}
+      style={{ width: width }}
+    >
+      {variant === "cartAction" && <img src={cartIcon} alt="Shopping cart" />}
+      {children}
+    </button>
+  );
+}
+
+export default ActionBtn;
