ad4d862f517b7aacd47ce0374f21d2688310d482
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-09-19 21:26:01 +0400

    Move ActionBtn to its own folder for better folder structure, add Form component template

--- src/components/Shared/ActionBtn/ActionBtn.jsx
+++ src/components/Shared/ActionBtn/ActionBtn.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "./ActionBtn.module.css";
-import cartIcon from "../../assets/shopping-cart-white.png";
+import cartIcon from "../../../assets/shopping-cart-white.png";
 
-function ActionBtn({ variant, children, width, size }) {
+function ActionBtn({ variant = null, children, width, size }) {
   return (
     <button
       className={size === "large" ? styles.btnLarge : styles.btnSmall}
---
+++ src/components/Shared/Forms/Form.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import styles from "./Form.module.css";
+import ActionBtn from "../ActionBtn/ActionBtn";
+
+function Form({ variant }) {
+  const renderForm = () => {
+    switch (variant) {
+      case "login":
+        return (
+          <>
+            <form action="">
+              <input type="email" placeholder="Email *" required />
+              <input type="password" placeholder="Password *" required />
+              <ActionBtn size="small">Log in</ActionBtn>
+            </form>
+            <p>
+              Not a member? <span>Register</span>
+            </p>
+          </>
+        );
+      case "registration":
+        return (
+          <>
+            <form action="">
+              <input type="file" />
+              <input type="text" placeholder="Username *" required />
+              <input type="email" placeholder="Email *" required />
+              <input type="password" placeholder="Password *" required />
+              <input
+                type="password"
+                placeholder="Confirm password *"
+                required
+              />
+              <ActionBtn size="small">Register</ActionBtn>
+            </form>
+            <p>
+              Already member? <span>Log in</span>
+            </p>
+          </>
+        );
+    }
+  };
+  return <div>{renderForm()}</div>;
+}
+
+export default Form;
