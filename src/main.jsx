24667a59c6f32ba2a49aa77a30b09f31b135a984
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-09-19 18:51:06 +0400

    remove basename from browserRouter for development period, add missing variable in color schemes

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/redseem-clothing/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
