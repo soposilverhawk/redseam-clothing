5fda041f398aaf1e72036fc83d656a4c542ffa02
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-09-19 19:55:26 +0400

    Add header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import logo from "../../assets/RedSeam-logo.png";
+import cartImg from "../../assets/shopping-cart-black.png";
+import userImg from "../../assets/user.png";
+import styles from "./Header.module.css";
+import { useNavigate } from "react-router-dom";
+import ROUTES from "../../routes/Routes";
 
 function Header() {
+  const navigate = useNavigate();
   return (
-    <div>
-      
-    </div>
-  )
+    <header className={styles.header}>
+      <button className={styles.logoContainer} onClick={() => navigate(ROUTES.HOME)}>
+        <img
+          src={logo}
+          alt="RedSeam Logo, orange hand with an eye inside it - "
+        />
+        <p>RedSeam Clothing</p>
+      </button>
+      <div className={styles.userPanelContainer}>
+        <button>
+          <img src={cartImg} alt="Shopping cart" />
+        </button>
+        <button onClick={() => navigate(ROUTES.LOGIN)}>
+          <img src={userImg} alt="Avatar icon" />
+          Log in
+        </button>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
