96c452c7c2776f842e6a2f362c64b2a85596e656
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-09-20 21:32:25 +0400

    add login form tracking states

--- src/components/Shared/Forms/Form.jsx
+++ src/components/Shared/Forms/Form.jsx
@@ -1,30 +1,76 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Form.module.css";
 import ActionBtn from "../ActionBtn/ActionBtn";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
 
-
 function Form({ variant }) {
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+  const [loginSubmittedData, setLoginSubmittedData] = useState(null);
+  const [isLoginSubmitted, setIsLoginSubmitted] = useState(false);
+  const handleLoginChange = (event) => {
+    const { name } = event.target;
+    setLoginData((prev) => ({
+      ...prev,
+      [name]: event.target.value,
+    }));
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setLoginSubmittedData(loginData);
+    setLoginData({
+      email: "",
+      password: "",
+    });
+  };
+  useEffect(() => {
+    if (loginSubmittedData) {
+      console.log("Submitted data", loginSubmittedData);
+    }
+  }, [loginSubmittedData]);
   const renderForm = () => {
     switch (variant) {
       case "login":
         return (
           <>
-            <form action="" className={styles.form}>
+            <form
+              action=""
+              className={styles.form}
+              onSubmit={(e) => handleSubmit(e)}
+            >
               <div className={styles.inputWrapper}>
-                <input type="email" required placeholder="" />
+                <input
+                  type="email"
+                  required
+                  placeholder=""
+                  name="email"
+                  value={loginData.email}
+                  onChange={(e) => handleLoginChange(e)}
+                />
                 <span className={styles.placeholder}>
                   Email <span className={styles.required}>*</span>
                 </span>
               </div>
 
               <div className={styles.inputWrapper}>
-                <input type="password" required placeholder="" />
+                <input
+                  type="password"
+                  required
+                  placeholder=""
+                  name="password"
+                  value={loginData.password}
+                  onChange={(e) => handleLoginChange(e)}
+                />
                 <span className={styles.placeholder}>
                   Password <span className={styles.required}>*</span>
                 </span>
-                <FontAwesomeIcon icon={faEye} className={styles.passwordVisibilityToggle}/>
+                <FontAwesomeIcon
+                  icon={faEye}
+                  className={styles.passwordVisibilityToggle}
+                />
               </div>
               <ActionBtn size="small">Log in</ActionBtn>
             </form>
@@ -55,18 +101,20 @@ function Form({ variant }) {
                 <span className={styles.placeholder}>
                   Password <span className={styles.required}>*</span>
                 </span>
-                <FontAwesomeIcon icon={faEye} className={styles.passwordVisibilityToggle}/>
+                <FontAwesomeIcon
+                  icon={faEye}
+                  className={styles.passwordVisibilityToggle}
+                />
               </div>
               <div className={styles.inputWrapper}>
-                <input
-                type="password"
-                placeholder=""
-                required
-              />
-              <span className={styles.placeholder}>
-                Confirm Password <span className={styles.required}>*</span>
-              </span>
-              <FontAwesomeIcon icon={faEye} className={styles.passwordVisibilityToggle}/>
+                <input type="password" placeholder="" required />
+                <span className={styles.placeholder}>
+                  Confirm Password <span className={styles.required}>*</span>
+                </span>
+                <FontAwesomeIcon
+                  icon={faEye}
+                  className={styles.passwordVisibilityToggle}
+                />
               </div>
               <ActionBtn size="small">Register</ActionBtn>
             </form>
