2f4fca5439dc1aeff21d76b1a596410033e5e597
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-09-22 13:09:46 +0400

    Add logic to hide cart button on login and registration pages

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,14 +3,18 @@ import logo from "../../assets/RedSeam-logo.png";
 import cartImg from "../../assets/shopping-cart-black.png";
 import userImg from "../../assets/user.png";
 import styles from "./Header.module.css";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import ROUTES from "../../routes/Routes";
 
 function Header() {
   const navigate = useNavigate();
+  const location = useLocation();
   return (
     <header className={styles.header}>
-      <button className={styles.logoContainer} onClick={() => navigate(ROUTES.HOME)}>
+      <button
+        className={styles.logoContainer}
+        onClick={() => navigate(ROUTES.HOME)}
+      >
         <img
           src={logo}
           alt="RedSeam Logo, orange hand with an eye inside it - "
@@ -18,9 +22,11 @@ function Header() {
         <p>RedSeam Clothing</p>
       </button>
       <div className={styles.userPanelContainer}>
-        <button>
-          <img src={cartImg} alt="Shopping cart" />
-        </button>
+        {location.pathname !== ROUTES.LOGIN && location.pathname !== ROUTES.REGISTER && (
+          <button>
+            <img src={cartImg} alt="Shopping cart" />
+          </button>
+        )}
         <button onClick={() => navigate(ROUTES.LOGIN)}>
           <img src={userImg} alt="Avatar icon" />
           Log in
