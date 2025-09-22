76c82cc74c5077f60aec88b41232e615b578721e
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-09-22 12:44:45 +0400

    Compose Login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Form from '../../components/Shared/Forms/Form';
+import FormHero from '../../components/Shared/FormHero/FormHero';
+import styles from "./Login.module.css"
 
 function Login() {
   return (
-    <div>
-      
-    </div>
+    <section className={styles.section}>
+      <FormHero />
+      <Form variant="login" />
+    </section>
   )
 }
 
