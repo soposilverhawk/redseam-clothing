8bfafd22c46049722872de8d79131d35e481de09
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-09-22 20:13:01 +0400

    Add default product cards with styles to homepage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,52 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import filtersIcon from "../../assets/filters-icon.png";
+import useProducts from "../../custom-hooks/useProducts";
+import styles from "./Home.module.css";
 
 function Home() {
+  const { data, loading, error } = useProducts({ page: 1 });
+  useEffect(() => {
+    if (data) console.log(data);
+  }, [data]);
   return (
-    <div>
-      
-    </div>
-  )
+    <section className={styles.productsSection}>
+      <div className={styles.productsHeader}>
+        <h1>Products</h1>
+        <div className={styles.userControlsContainer}>
+          <p>
+            Showing{" "}
+            <span>
+              {data?.meta?.from} - {data?.meta?.to}
+            </span>{" "}
+            of <span>{data?.meta?.total}</span> results
+          </p>
+          <button>
+            <img src={filtersIcon} alt="Filters option icon" />
+            Filter
+          </button>
+          <select name="sorting" id="sorting">
+            {/* handle values once you'll move onto sorting functionality */}
+            <option value="">Sort by</option>
+            <option value="">New products first</option>
+            <option value="">Price, low to high</option>
+            <option value="">Price, high to low</option>
+          </select>
+        </div>
+      </div>
+      <div className={styles.productsContainer}>
+        {data?.data?.map(({ id, cover_image, name, description, price }) => (
+          <button key={id}>
+              <img src={cover_image} alt={description} />
+
+            <div className={styles.productInformation}>
+              {name}
+              <p>$ {price}</p>
+            </div>
+          </button>
+        ))}
+      </div>
+    </section>
+  );
 }
 
-export default Home
-
+export default Home;
