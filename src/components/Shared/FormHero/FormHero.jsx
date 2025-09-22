7310acd83392998cd6acb3d6e686653322d5b7a1
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-09-22 12:35:23 +0400

    Add form hero component

---
+++ src/components/Shared/FormHero/FormHero.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import formHero from "../../../assets/form-hero.png";
+import styles from "./FormHero.module.css";
+
+function FormHero() {
+  return <div className={styles.heroContainer}></div>;
+}
+
+export default FormHero;
