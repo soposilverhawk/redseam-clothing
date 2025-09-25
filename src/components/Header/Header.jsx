b84218a9a01970317b03456e3560b019e2c888a8
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-09-26 02:23:17 +0400

    add logic to dispaly user image in header if the img is present

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,10 +5,14 @@ import userImg from "../../assets/user.png";
 import styles from "./Header.module.css";
 import { useNavigate, useLocation } from "react-router-dom";
 import ROUTES from "../../routes/Routes";
+import { useAuth } from "../../context/AuthContext";
+import userIcon from "../../assets/user.png";
 
 function Header() {
   const navigate = useNavigate();
   const location = useLocation();
+  const { user, token } = useAuth();
+  console.log(user);
   return (
     <header className={styles.header}>
       <button
@@ -22,15 +26,28 @@ function Header() {
         <p>RedSeam Clothing</p>
       </button>
       <div className={styles.userPanelContainer}>
-        {location.pathname !== ROUTES.LOGIN && location.pathname !== ROUTES.REGISTER && (
-          <button>
-            <img src={cartImg} alt="Shopping cart" />
+        {location.pathname !== ROUTES.LOGIN &&
+          location.pathname !== ROUTES.REGISTER && (
+            <button>
+              <img src={cartImg} alt="Shopping cart" />
+            </button>
+          )}
+        {!user && !token ? (
+          <button onClick={() => navigate(ROUTES.LOGIN)}>
+            <img src={userImg} alt="Avatar icon" />
+            Log in
           </button>
+        ) : user?.avatar ? (
+          <img
+            src={user.avatar}
+            alt="user avatar"
+            className={styles.userAvatar}
+          />
+        ) : (
+          <div className={styles.userAvatarDefault}>
+            <img src={userIcon} alt="default userIcon" />
+          </div>
         )}
-        <button onClick={() => navigate(ROUTES.LOGIN)}>
-          <img src={userImg} alt="Avatar icon" />
-          Log in
-        </button>
       </div>
     </header>
   );
