c821fa4b3c61e91c33b46acb4e44945fc840b622
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-09-26 04:15:08 +0400

    add updateCart functionality to useCart custom hook

--- src/custom-hooks/useCart.jsx
+++ src/custom-hooks/useCart.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
-import { useAuth } from "../context/AuthContext"; 
+import { useAuth } from "../context/AuthContext";
 
 function useCart() {
   const { token } = useAuth();
@@ -23,6 +23,32 @@ function useCart() {
         }
       );
 
+      return response.data;
+    } catch (err) {
+      setError(err.response?.data || err.message);
+      throw err;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const updateCart = async ({ productId, quantity }) => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await axios.patch(
+        `https://api.redseam.redberryinternship.ge/api/cart/products/${productId}`,
+        {
+          quantity,
+        },
+        {
+          headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
       return response.data;
     } catch (err) {
       setError(err.response?.data || err.message);
