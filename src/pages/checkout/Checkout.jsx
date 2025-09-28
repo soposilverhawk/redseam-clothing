efbb2804379cd690d10c48635ba2ee0f7837a3ee
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-09-28 14:45:58 +0400

    add checkout form inputs state tracking

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Checkout.module.css";
-import CartInfo from "../../components/Shared/Cart/CartInfo";
+import { useAuth } from "../../context/AuthContext";
 
 function Checkout() {
+  const { user } = useAuth();
+  const [orderDetails, setOrderDetails] = useState({
+    name: "",
+    surname: "",
+    email: user?.email || "",
+    address: "",
+    zip_code: "",
+  });
+  const handleInputChange = (e) => {
+    const {name, value} = e.target;
+    setOrderDetails((prev) => ({
+      ...prev,
+      [name]: value
+    }))
+  }
   return (
     <section className={styles.section}>
       <h1>Checkout</h1>
@@ -12,15 +27,50 @@ function Checkout() {
           <h2>Order details</h2>
           <form>
             <div className={styles.inputGroup}>
-              <input type="text" placeholder="Name"/>
-              <input type="text" placeholder="Surname"/>
+              <input
+                type="text"
+                placeholder="Name"
+                name="name"
+                value={orderDetails.name}
+                onChange={(e) => handleInputChange(e)}
+                required
+              />
+              <input
+                type="text"
+                placeholder="Surname"
+                name="surname"
+                value={orderDetails.surname}
+                onChange={(e) => handleInputChange(e)}
+                required
+              />
             </div>
             <div className={styles.inputGroup}>
-              <input type="email" placeholder="Email"/>
+              <input
+                type="email"
+                placeholder="Email"
+                name="email"
+                value={orderDetails.email}
+                onChange={(e) => handleInputChange(e)}
+                required
+              />
             </div>
             <div className={styles.inputGroup}>
-              <input type="text" placeholder="Address"/>
-              <input type="text" placeholder="Zip Code"/>
+              <input
+                type="text"
+                placeholder="Address"
+                name="address"
+                value={orderDetails.address}
+                onChange={(e) => handleInputChange(e)}
+                required
+              />
+              <input
+                type="text"
+                placeholder="Zip Code"
+                name="zip_code"
+                value={orderDetails.zip_code}
+                onChange={(e) => handleInputChange(e)}
+                required
+              />
             </div>
           </form>
         </div>
