f1adaca9c196d33b4c414f298ed191fcfdeb522e
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-09-28 14:18:25 +0400

    Add checkout page layout and form visual representation

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import styles from "./Checkout.module.css";
+import CartInfo from "../../components/Shared/Cart/CartInfo";
 
 function Checkout() {
   return (
-    <div>
-      
-    </div>
-  )
+    <section className={styles.section}>
+      <h1>Checkout</h1>
+      {/* content container */}
+      <div className={styles.contentWrapper}>
+        <div className={styles.formWrapper}>
+          <h2>Order details</h2>
+          <form>
+            <div className={styles.inputGroup}>
+              <input type="text" placeholder="Name"/>
+              <input type="text" placeholder="Surname"/>
+            </div>
+            <div className={styles.inputGroup}>
+              <input type="email" placeholder="Email"/>
+            </div>
+            <div className={styles.inputGroup}>
+              <input type="text" placeholder="Address"/>
+              <input type="text" placeholder="Zip Code"/>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Checkout
+export default Checkout;
